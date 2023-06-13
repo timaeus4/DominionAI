981c04df4a8498b948a0b7e90a5667a258054a82
bittbyte <130473154+bittbyte@users.noreply.github.com>
2023-06-13 23:32:17 +0900

    Update action_card_test.py
    add test_vassal, test_archive

--- test/Dominion/cardlist/action_card_test.py
+++ test/Dominion/cardlist/action_card_test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("..\src")
+sys.path.append("../src")
 
 import unittest
 from Dominion import player
@@ -8,7 +8,8 @@ from Dominion.card.cardlist import victory_card
 from Dominion.card.cardlist import money_card
 from Dominion.card.cardlist import card
 
-plain_card = card.card("", "", "", "", 0, 0, 0, 0, "N")
+plain_action_card = card.card("", "", "action", 0, 0, 0, 0, "N")
+plain_money_card = card.card("", "", "money", 0, 0, 0, 0, "N")
 
 class test_village(unittest.TestCase):
     def test_effect(self):
@@ -16,7 +17,7 @@ class test_village(unittest.TestCase):
         
         supply = []
         t = player.Player()
-        t.deck = [plain_card]
+        t.deck = [plain_action_card]
         t.hand = []
         t.play_area = []
         t.discard = []
@@ -26,13 +27,61 @@ class test_village(unittest.TestCase):
         action_stack = 0
         
         exp_deck = []
-        exp_hand = [plain_card]
+        exp_hand = [plain_action_card]
         exp_actionstack = 2
         
         _, act_t, _, _, act_actionstack = test_class.effect(supply, t, None, trash, action_stack)
         self.assertEqual(exp_deck, act_t.deck)
         self.assertEqual(exp_hand, act_t.hand)
         self.assertEqual(exp_actionstack,act_actionstack)
-    
+
+class test_vassal(unittest.TestCase):
+    def test_effect(self):
+        test_class=action_card.vassal()
+        
+        supply = []
+        t = player.Player()
+        t.deck = []
+        t.hand = []
+        t.play_area = []
+        t.discard = []
+        t.money_stack = 0
+        t.buy_stack = 0
+        trash = []
+        action_stack = 0
+        
+        exp_deck = []
+        exp_hand = []
+        exp_money_stack = 2
+        
+        _, act_t, _, _, act_actionstack = test_class.effect(supply, t, None, trash, action_stack)
+        self.assertEqual(exp_deck, act_t.deck)
+        self.assertEqual(exp_hand, act_t.hand)
+        self.assertEqual(exp_money_stack,act_t.money_stack)
+  
+class test_archive(unittest.TestCase):
+    def test_effect(self):
+        test_class=action_card.archive()
+        
+        supply = []
+        t = player.Player()
+        t.deck = [plain_money_card]
+        t.hand = []
+        t.play_area = []
+        t.discard = []
+        t.money_stack = 0
+        t.buy_stack = 0
+        trash = []
+        action_stack = 0
+        
+        exp_deck = []
+        exp_hand = [plain_money_card]
+        exp_discard = []     
+        
+        _, act_t, _, _, act_actionstack = test_class.effect(supply, t, None, trash, action_stack)
+        self.assertEqual(exp_deck, act_t.deck)
+        self.assertEqual(exp_hand, act_t.hand)
+        self.assertEqual(exp_discard,act_t.discard)
+
 if __name__ == '__main__':
     unittest.main()
